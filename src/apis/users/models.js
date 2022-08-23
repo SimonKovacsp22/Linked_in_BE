97944bffc1cd87979e7e5b8feaca62ddff0dbc5b
SidathDabare <sidath2007@gmail.com>
2022-08-23 19:57:50 +0200

    comment

--- src/apis/users/models.js
+++ src/apis/users/models.js
@@ -9,12 +9,26 @@ const usersSchema = new Schema(
     name: { type: String, required: true },
     surname: { type: String, required: true },
     email: { type: String, required: true },
+    password: { type: String, required: true },
     bio: { type: String, required: true },
     title: { type: String, required: true },
     area: { type: String, required: true },
     image: { type: String, required: true },
     username: { type: String, required: true },
+
     //dateOfBirth: { type: Date, required: true },
+
+    experiences: [
+      {
+        role: String,
+        company: String,
+        description: String,
+        startDate: String,
+        endDate: String,
+        area: String,
+        imageUrl: String,
+      },
+    ],
   },
   {
     timestamps: true,
